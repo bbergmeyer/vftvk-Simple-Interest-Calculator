b78d3433507808cef7638f0f67866fce78fb4d4c
bbergmeyer <b_bergmeyer@hotmail.com>
2022-03-02 21:54:29 -0600

    update to meet requirements

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    const prin = document.getElementById("principal");
+    const rate = document.getElementById("rate");
+    const years = document.getElementById("years");
+    if (prin.value == "" || prin.value < 1) {
+        alert("Amount must be greater than 0");
+        prin.focus();
+        return;
+    }
+
+    if (years.value == "" || years.value < 1) {
+        alert("Number of years must be greater than 0");
+        prin.focus();
+        return;
+    }
+    const actualYears = years.value;    
+    let futureYear = (Number(new Date().getFullYear().toString()) *1) + (actualYears *1);
+    const amount = prin.value * (rate.value/100) * years.value;
+
+
+    const newElement = 
+    `
+If you deposit <mark>${prin.value}</mark>,
+at an interest rate of <mark>${rate.value}%</mark>.
+You will receive an amount of <mark>${amount}</mark>,
+in the year <mark>${futureYear}</mark>`;
+
+document.getElementById("solution").innerHTML = newElement;
+                 
+
     
 }
         
